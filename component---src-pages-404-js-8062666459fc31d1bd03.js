57b9689b267c2c8fda069a8d39006e1bf15e23d8
Carbon Copy <cc.glows@gmail.com>
2019-08-04 10:17:02 +1000

    Updates

--- component---src-pages-404-js-8062666459fc31d1bd03.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{209:function(e,t,n){"use strict";n.r(t);var o=n(10),a=n.n(o),r=n(0),u=n.n(r),l=n(225),p=n(228);t.default=function(e){var t=e.pageContext,n=a()({},t,{frontmatter:{title:"404: Not Found"}});return u.a.createElement(l.a,{pageContext:n},u.a.createElement(p.a,{title:"404: Not found",pageContext:n}),u.a.createElement("p",null,"Unfortunately, the page you were looking for does not exist."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-8062666459fc31d1bd03.js.map
